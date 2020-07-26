5baf1225c3282b87bdae4b58b7ada67d259d2944
brutekoon <brutekoon@gmail.com>
2020-07-27 02:09:32 +0900

    Clinet (Android Format)

---
+++ Client/Java_Code/app/src/main/java/com/example/serverboy_client/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.serverboy_client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("test");
+
+
+    }
+}
