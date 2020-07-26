ff79013480eac6aaf9536fc75ef3e1f31ef78b13
brutekoon <brutekoon@gmail.com>
2020-07-27 02:10:02 +0900

    Clinet (Android Format)

--- Client/Java_Code/app/src/main/java/com/example/serverboy_client/MainActivity.java
+++ Client/Java_Code/app/src/main/java/com/example/serverboy_client/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("test");
 
 
     }
