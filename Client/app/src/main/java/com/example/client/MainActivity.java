641f8d7162ef4c02884cc2ec898ba5def34f96d6
brutekoon <brutekoon@gmail.com>
2020-08-10 01:33:52 +0900

    Login & Register Page Sample ( but error!!!)

---
+++ Client/app/src/main/java/com/example/client/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.client;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_id, tv_pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String userID = intent.getStringExtra("userID");
+        String userPass = intent.getStringExtra("userPass");
+
+        tv_id.setText(userID);
+        tv_pass.setText(userPass);
+    }
+}
