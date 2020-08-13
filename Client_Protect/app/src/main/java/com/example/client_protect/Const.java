e5237eedeb7f05a2a0b378654dd7bb7e56497a04
brutekoon <brutekoon@gmail.com>
2020-08-13 16:37:43 +0900

    Checker Code add

---
+++ Client_Protect/app/src/main/java/com/example/client_protect/Const.java
@@ -0,0 +1,88 @@
+package com.example.client_protect;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Const {
+    public static final String BINARY_SU = "su";
+
+    public static final String[] knownRootAppsPackages = {
+            "com.noshufou.android.su",
+            "com.noshufou.android.su.elite",
+            "eu.chainfire.supersu",
+            "com.koushikdutta.superuser",
+            "com.thirdparty.superuser",
+            "com.yellowes.su",
+            "com.topjohnwu.magisk",
+            "com.kingroot.kinguser",
+            "com.kingo.root",
+            "com.smedialink.oneclickroot",
+            "com.zhiqupk.root.global",
+            "com.alephzain.framaroot"
+    };
+
+    public static final String[] knownDangerousAppsPackages = {
+            "com.koushikdutta.rommanager",
+            "com.koushikdutta.rommanager.license",
+            "com.dimonvideo.luckypatcher",
+            "com.chelpus.lackypatch",
+            "com.ramdroid.appquarantine",
+            "com.ramdroid.appquarantinepro",
+            "com.android.vending.billing.InAppBillingService.COIN",
+            "com.chelpus.luckypatcher"
+    };
+
+    public static final String[] pathsThatShouldNotBeWritable = {
+            "/system",
+            "/system/bin",
+            "/system/sbin",
+            "/system/xbin",
+            "/vendor/bin",
+            "/sbin",
+            "/etc",
+            //"/sys",
+            //"/proc",
+            //"/dev"
+    };
+
+    public static final String[] suPaths ={
+            "/data/local/",
+            "/data/local/bin/",
+            "/data/local/xbin/",
+            "/sbin/",
+            "/su/bin/",
+            "/system/bin/",
+            "/system/bin/.ext/",
+            "/system/bin/failsafe/",
+            "/system/sd/xbin/",
+            "/system/usr/we-need-root/",
+            "/system/xbin/",
+            "/cache/",
+            "/data/",
+            "/dev/"
+    };
+
+    static String[] getPaths(){
+        ArrayList<String> paths = new ArrayList<>(Arrays.asList(suPaths));
+
+        String sysPaths = System.getenv("PATH");
+
+        // If we can't get the path variable just return the static paths
+        if (sysPaths == null || "".equals(sysPaths)){
+            return paths.toArray(new String[0]);
+        }
+
+        for (String path : sysPaths.split(":")){
+
+            if (!path.endsWith("/")){
+                path = path + '/';
+            }
+
+            if (!paths.contains(path)){
+                paths.add(path);
+            }
+        }
+
+        return paths.toArray(new String[0]);
+    }
+}
---
+++ Client_Protect/app/src/main/java/com/example/client_protect/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.client_protect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Context mContext = null;
+
+        // Make RootChecker Object
+        final Checker checker = new Checker(mContext);
+
+        final TextView logview = findViewById(R.id.LogView);
+
+
+        // Check_button1
+        Button button_1 = findViewById(R.id.Check_Button1);
+        button_1.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean result = checker.checkForDangerousProps();
+                logview.setText(String.valueOf(result));
+            }
+        });
+
+    }
+}
